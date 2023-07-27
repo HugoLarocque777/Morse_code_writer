db3d7066843ee3f14957d8c300a45d50eab3d59d
Hugo <140665128+HugoLarocque777@users.noreply.github.com>
2023-07-27 12:06:58 -0600

    Add files via upload

---
+++ Morse Code.py
@@ -0,0 +1,119 @@
+#Leo helped a tiny bit with this program go to his github "LeoLarocque" account for awesome code!
+print ("Type in your message one character at a time.")
+print ("Your message will be converted into morse code.")
+print ("Type done when your message is complete")
+print (". = dot, and _ = dash")
+while 1 == 1:
+    code = input()
+    if code == "a":
+        print ("a equals ._  ")
+
+    elif code == "b":
+        print ("b equals _...  ")
+        
+    elif code == "c":
+        print ("c equals _._.  ")
+
+    elif code == "d":
+        print ("d equals _..  ")
+
+    elif code == "e":
+        print ("e equals .  ")
+
+    elif code == "f":
+        print ("f equals .._.  ")
+
+    elif code == "g":
+        print ("g equals _ _.  ")
+        
+    elif code == "h":
+        print ("h equals ....  ")
+        
+    elif code == "i":
+        print ("i equals ..  ")
+
+    elif code == "j":
+        print ("j equals ._ _ _  ")
+
+    elif code == "k":
+        print ("k equals _._  ")
+
+    elif code == "l":
+        print ("l equals ._..  ")
+
+    elif code == "m":
+        print ("m equals _ _  ")
+        
+    elif code == "n":
+        print ("n equals _.  ")
+        
+    elif code == "o":
+        print ("o equals _ _ _  ")
+
+    elif code == "p":
+        print ("p equals ._ _.  ")
+
+    elif code == "q":
+        print ("q equals _ _._  ")
+
+    elif code == "r":
+        print ("r equals ._.  ")
+
+    elif code == "s":
+        print ("s equals ...  ")
+        
+    elif code == "t":
+        print ("t equals _ _ _  ")
+        
+    elif code == "u":
+        print ("u equals .._  ")
+
+    elif code == "v":
+        print ("v equals ..._  ")
+
+    elif code == "w":
+        print ("w equals ._ _  ")
+
+    elif code == "x":
+        print ("x equals _.._  ")
+
+    elif code == "y":
+        print ("y equals _._ _  ")
+
+    elif code == "z":
+        print ("z equals _ _._  ")
+
+    elif code == "0":
+        print ("0 equals _ _ _ _ _  ")
+
+    elif code == "1":
+        print ("1 equals ._ _ _ _  ")
+
+    elif code == "2":
+        print ("2 equals .. _ _ _  ")
+        
+    elif code == "3":
+        print ("3 equals ... _ _  ")
+        
+    elif code == "4":
+        print ("4 equals ...._  ")
+        
+    elif code == "5":
+        print ("5 equals .....  ")
+
+    elif code == "6":
+        print ("6 equals _.... ")
+
+    elif code == "7":
+        print ("7 equals _ _... ")
+
+    elif code == "8":
+        print ("8 equals _ _ _..  ")
+
+    elif code == "9":
+        print ("9 equals _ _ _ _.  ")
+    elif code == "done":
+        print("Goodbye!")
+        break
+
+
